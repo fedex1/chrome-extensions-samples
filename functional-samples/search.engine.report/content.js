a80f7f57eb5bdfa9b11c3e0df8836583e17fa7d0
ralph yozzo <ralph@brooklynmarathon.com>
2024-02-12 22:32:23 -0500

    ralph-desktop

---
+++ functional-samples/search.engine.report/content.js
@@ -0,0 +1,7 @@
+        let message;
+        if (document.querySelector("link[rel='canonical']")) {
+            message = (`canonical link: ${document.querySelector("link[rel='canonical']").getAttribute("href")}`);
+        } else {
+            message = (`no canonical link: ${document.body}`);
+        }
+        alert(message);
